4de8b1ca28b78128b08035b3364dd7e4c896ed40
Mark Lindan <trevel@gmail.com>
2013-07-05 10:53:27 -0400

    Add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   root to: "static_pages#home"
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: "static_pages#about",   via: 'get'
-  match '/contact', to: "static_pages#contact", via: 'get'
+  match '/help',    via: 'get', to: 'static_pages#help'
+  match '/about',   via: 'get', to: "static_pages#about"
+  match '/contact', via: 'get', to: "static_pages#contact"
+  match '/signup',  via: 'get', to: "users#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
